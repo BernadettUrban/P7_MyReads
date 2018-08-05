85bf7de2a21374339d488f8e37d1b72cf687fa97
Bernadett Urban <bernadett.urban@hotmail.com>
2018-08-05 15:24:12 +0200

    feat: shelf update would reflect on search results

--- src/search-page.js
+++ src/search-page.js
@@ -17,19 +17,32 @@ class SearchPage extends Component {
     state = {
         results: []
       }
+
+      updateResults = (updatedBook, shelf) => {
+        const index = this.state.results.indexOf(updatedBook);
+        const newBook = Object.assign({}, updatedBook, { shelf: shelf });
+        this.setState({
+          results: [
+            ...this.state.results.slice(0, index),
+            newBook,
+            ...this.state.results.slice(index + 1),
+          ]
+        });
+      }
+
        searchBooks = (event) => {
         const query = event.target.value.trim();
         if (query.length >= 3) {
           BooksAPI.search(query, maxResults)
-          .then(response => {
-            this.props.books.forEach(shelvedBook => {
-              const match = response.find((resultBook) => resultBook.id === shelvedBook.id);
-              if (match) {
-                match.shelf = shelvedBook.shelf;  
-              }
-            })
-            this.setState({ results: response });
-        });
+            .then(response => {
+              this.props.books.forEach(shelvedBook => {
+                const match = response.find((resultBook) => resultBook.id === shelvedBook.id);
+                if (match) {
+                  match.shelf = shelvedBook.shelf;  
+                }
+              })
+              this.setState({ results: response });
+          });
         }
       }
 
@@ -51,7 +64,10 @@ class SearchPage extends Component {
           <div className="search-books-results">
             <BookList 
               list={this.state.results}
-              handleShelfUpdate={this.props.handleShelfUpdate}
+              handleShelfUpdate={(book, value) => {
+                this.props.handleShelfUpdate(book, value);
+                this.updateResults(book, value);
+              }}
             />
           </div> 
         )}
