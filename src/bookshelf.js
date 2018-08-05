06c319ae32d8085e4794531ab586e7dee3b8c3b7
Bernadett Urban <bernadett.urban@hotmail.com>
2018-08-05 15:19:30 +0200

    refactor: reduce code duplication

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types';
  import BookList from 'book-list';
  const BookShelf = (props) => (
   <div className="bookshelf">
-    <h2 className="bookshelf-title">Currently Reading</h2>
+    <h2 className="bookshelf-title">{props.name}</h2>
     <div className="bookshelf-books">
       <BookList 
         list={props.books}
